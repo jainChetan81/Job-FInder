9d4ab23d8b10bf64f74fc6d725ea43dc93bad06d
jainChetan81 <jain.cj.chetan@gmail.com>
2021-06-12 16:29:00 +0530

    Updates

---
+++ precache-manifest.d2b39813c55c3dc125b049358c48eb11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2aa69d8930073bdeb0de0e654f06907",
+    "url": "/Job-FInder/index.html"
+  },
+  {
+    "revision": "38c10e4fdb02a704db71",
+    "url": "/Job-FInder/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "955d6e40e94f4abb6de2",
+    "url": "/Job-FInder/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "38c10e4fdb02a704db71",
+    "url": "/Job-FInder/static/js/2.ea2d68d9.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/Job-FInder/static/js/2.ea2d68d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "955d6e40e94f4abb6de2",
+    "url": "/Job-FInder/static/js/main.4ee7add5.chunk.js"
+  },
+  {
+    "revision": "257f54a55c3b56e48f48",
+    "url": "/Job-FInder/static/js/runtime-main.a1ecadea.js"
+  }
+]);
